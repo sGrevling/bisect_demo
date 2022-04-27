b5f648b03095f8d6f659423782b0c13372692250
sigvegodoy <sigvegrevling@gmail.com>
2022-04-27 13:40:40 +0200

    eject and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [shoModal, setShoModal] = useState(false);
+    const version = 0;
+    const buttonBug = null;
+
+    const renderModal = () => shoModal && !buttonBug && (
+        <div id="fantastic-modal-wrapper">
+            <div id="fantastic-modal">
+                <h1>Fantastic modal</h1>
+                <p>The modal brings great joy to users, mainly because of the beautiful colour.</p>
+            </div>
+            <div
+                id="fantastic-modal-outside"
+                onClick={() => setShoModal(false)}
+            />
+        </div>
+    );
+
+    const renderButton = () => (
+        <button
+            id="fantastic-button"
+            onClick={() => setShoModal(true)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            Fantastic button
+        </button>
+    )
+
+    return (
+        <div id="app">
+            <h1>Fantastic App versjon {version}</h1>
+            {renderModal()}
+            {renderButton()}
+            <div id="other-content">
+                <p>More</p>
+                <p>perfectly</p>
+                <p>mediocre </p>
+                <p>content</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
+/* eslint-disable testing-library/no-node-access */
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('renders_header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/fantastic app/i))
+      .toBeInTheDocument();
+});
+
+test('fantastic_modal_works', () => {
+  render(<App />);
+  const button = screen.getByText(/fantastic button/i);
+  expect(button).toBeInTheDocument();
+  userEvent.click(button);
+  expect(screen.getByText(/fantastic modal/i))
+      .toBeInTheDocument();
 });
