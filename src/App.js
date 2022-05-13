afcf656e2d0bdd92b3921e0c26174d91d72bd0dd
sigvegodoy <sigvegrevling@gmail.com>
2022-05-13 11:13:57 +0200

    v29
    (cherry picked from commit 95eecdf5880c431758846ae81674096bf7eabae3)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useState} from "react";
 
 function App() {
     const [shoModal, setShoModal] = useState(false);
-    const version = 28;
+    const version = 29;
     const buttonBug = '🐛';
 
     const renderModal = () => shoModal && !buttonBug && (
