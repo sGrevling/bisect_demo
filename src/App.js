354b638a4c6e5379624aae58414300e0c5ea3007
sigvegodoy <sigvegrevling@gmail.com>
2022-12-29 16:49:28 +0100

    V4
    (cherry picked from commit 708f3987b17d77ecd664810e69b91f5916045157)
    (cherry picked from commit f514bbe9adcef1eb275bb25eac9b46b846636fe3)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BinarySearch} from "./BinarySearch";
 
 function App() {
     const [fantastic, setFantastic] = useState(1);
-    const version = 3;
+    const version = 4;
     const buttonBug = null;
 
     return (
